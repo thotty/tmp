aab791c8993f5e8f4163edef2cda7309d0f726fa
Sósthenes Neto <thotty@users.noreply.github.com>
2022-05-31 12:57:51 -0300

    Update render-list-with-delay.ts

--- stuffs/render-list-with-delay.ts
+++ stuffs/render-list-with-delay.ts
@@ -31,3 +31,20 @@ const list: Item[] = [
 list.forEach((item) => {
   render(item);
 });
+
+/*
+output:
+
+12:38:31: loading...
+12:38:31: msg: oi, sou o primeiro item
+12:38:31: loading...
+12:38:32: msg: e eu sou o segundo
+12:38:32: loading...
+12:38:35: msg: aqui é o terceiro e preciso de uma pausa de 3s
+12:38:35: loading...
+12:38:35: msg: 4º item e vou rodar depois do 3º
+12:38:35: loading...
+12:38:41: msg: quinto item e com uma pausa maior: 5s
+12:38:41: loading...
+12:38:41: msg: ultimo item!
+*/
